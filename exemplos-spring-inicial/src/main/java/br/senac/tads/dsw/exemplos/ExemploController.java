ce41f7da11a847e204060bf1c58223719b7618d7
ftsuda-senac <ftsuda.senac@gmail.com>
2025-09-04 22:25:10 -0300

    Versão final do Controller

--- exemplos-spring-inicial/src/main/java/br/senac/tads/dsw/exemplos/ExemploController.java
+++ exemplos-spring-inicial/src/main/java/br/senac/tads/dsw/exemplos/ExemploController.java
@@ -2,32 +2,18 @@ package br.senac.tads.dsw.exemplos;
 
 import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class ExemploController {
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @GetMapping(produces = "application/json")
-    public String gerarJson(
+    public Dados gerarJson(
             @RequestParam("nome") String nome,
             @RequestParam("email") String email,
             @RequestParam("time") String time) {
-        Dados dados = new Dados(nome, email, time, "xpto1234", LocalDateTime.now());
-        try {
-            return objectMapper.writeValueAsString(dados);
-        } catch (JsonProcessingException e) {
-            return "{}";
-        }
+        return new Dados(nome, email, time, "abcdq121", LocalDateTime.now());
     }
 }
