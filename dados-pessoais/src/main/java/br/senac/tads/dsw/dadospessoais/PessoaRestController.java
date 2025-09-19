3d71a9929611fab8c0f7aad1cc0464bc952f24f0
ftsuda-senac <ftsuda.senac@gmail.com>
2025-09-18 22:22:39 -0300

    Inclusão do Swagger, POST, PUT e DELETE

--- dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/PessoaRestController.java
+++ dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/PessoaRestController.java
@@ -1,15 +1,27 @@
 package br.senac.tads.dsw.dadospessoais;
 
+import java.net.URI;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
+@RequestMapping("/pessoas")
 public class PessoaRestController {
 
     @Autowired
@@ -33,7 +45,41 @@ public class PessoaRestController {
 
     @GetMapping("/{username}")
     public Pessoa findByUsername(@PathVariable("username") String u) {
-        return service.findByUsername(u);
+        Pessoa p = service.findByUsername(u);
+        if (p == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "Usuário %s não encontrado".formatted(u));
+        }
+        return p;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addNew(@RequestBody Pessoa pessoa) {
+        Pessoa p = service.addNew(pessoa);
+        URI location = ServletUriComponentsBuilder
+            .fromCurrentRequestUri()
+            .path("/{username}")
+            .buildAndExpand(p.getUsername())
+            .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/{username}")
+    public ResponseEntity<?> update(
+            @PathVariable String username,
+            @RequestBody Pessoa pessoa) {
+        Pessoa p = service.findByUsername(username);
+        if (p == null) {
+            return ResponseEntity.notFound().build();
+        }
+        p = service.update(username, pessoa);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{username}")
+    public ResponseEntity<?> delete(@PathVariable String username) {
+        service.delete(username);
+        return ResponseEntity.noContent().build();
     }
 
 }
