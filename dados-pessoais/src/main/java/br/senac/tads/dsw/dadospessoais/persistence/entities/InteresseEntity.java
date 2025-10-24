29e372f2eb3f63a8caca373fdae396202d17651a
ftsuda-senac <ftsuda.senac@gmail.com>
2025-10-23 21:38:21 -0300

    Configuração do BD e criação entities

---
+++ dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/persistence/entities/InteresseEntity.java
@@ -0,0 +1,39 @@
+package br.senac.tads.dsw.dadospessoais.persistence.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "interesse")
+public class InteresseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+
+}
